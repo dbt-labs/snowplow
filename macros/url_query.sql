d94636556d201d5d03229ad7dea6f4754b375966
Tristan Handy <jthandy.gmail.com>
2017-08-15 13:44:05 -0400

    fix empty string utm parameters when resessionizing from internal referrers

--- macros/url_query.sql
+++ macros/url_query.sql
@@ -1,6 +1,10 @@
 
 {% macro get_utm_parameter(query_field, utm_param) -%}
 
-split_part(split_part({{ query_field }}, '{{ utm_param }}=', 2), '&', 1)
+nullif(
+    split_part(
+        split_part({{ query_field }}, '{{ utm_param }}=', 2),
+        '&', 1),
+    '')
 
 {%- endmacro %}
