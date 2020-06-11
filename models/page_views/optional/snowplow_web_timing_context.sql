6f02a08e9981ff5150caa382acb98e354c3314f4
Jeremy Cohen <jeremy@fishtownanalytics.com>
2020-06-11 15:41:16 -0400

    Cross-compat filter in web timing

--- models/page_views/optional/snowplow_web_timing_context.sql
+++ models/page_views/optional/snowplow_web_timing_context.sql
@@ -75,9 +75,13 @@ prep as (
       and pt.load_event_end is not null -- zero is acceptable
 
       -- remove rare outliers (Unix timestamp is more than twice what it should be)
-      and datediff(d, pt.root_tstamp, (timestamp 'epoch' + pt.response_end/1000 * interval '1 second ')) < 365
-      and datediff(d, pt.root_tstamp, (timestamp 'epoch' + pt.unload_event_start/1000 * interval '1 second ')) < 365
-      and datediff(d, pt.root_tstamp, (timestamp 'epoch' + pt.unload_event_end/1000 * interval '1 second ')) < 365
+      
+      {% set ts_columns = ['pt.response_end', 'pt.unload_event_start', 'pt.unload_event_end'] %}
+      {% for ts_column in ts_columns %}
+      
+      and {{ dbt_utils.datediff(dbt_utils.dateadd('millisecond', ts_column, "'1970-01-01'"), 'pt.root_tstamp', 'day') }} < 365
+      
+      {% endfor %}
 
 ),
 
